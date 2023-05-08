15b45e89bc8fbe074dbdc701583bfa13b6254a28
AaishaDev <aaishllap@gmail.com>
2023-05-08 16:31:05 +0530

    origin added

--- back-end/config/allowedOrigin.js
+++ back-end/config/allowedOrigin.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ['http://127.0.0.1:5500', 'http://127.0.0.1:5500/'];
+const allowedOrigins = ['http://127.0.0.1:5500', 'http://127.0.0.1:5500/', 'https://bday-reminderr.netlify.app/','https://bday-reminderr.netlify.app'];
 
 module.exports = {
   allowedOrigins
